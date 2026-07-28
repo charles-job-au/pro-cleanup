0f0cbe5b013e6c1c585f34ec51178764ac9790ca
Charles <charles-job-au@outlook.com>
2026-07-28 21:50:45 +0800

    fix next assets path for github pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
+const repo = "pro-cleanup";
+
 const nextConfig: NextConfig = {
   output: "export",
+
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
+
   images: {
     unoptimized: true,
   },
